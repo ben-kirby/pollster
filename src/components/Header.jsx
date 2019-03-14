c647a039461504ba0c1ffcc204c211b9367bd37f
Ben Kirby <benkrby@gmail.com>
2019-03-13 21:40:07 -0700

    Styled header and added links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: ['Josefin Sans:600i', 'sans-serif']
+  }
+});
+
+
+
+const styles = {
+  container: styled.div`
+    display: flex;
+    margin: 15px 0 0 15px;
+
+  `,
+  link: {
+    marginRight: '10px',
+    textDecoration: 'none',
+    fontFamily: 'Josefin Sans',
+    color: '#275DAD',
+  }
+};
 
 function Header() {
   return(
-    <div>
-      <Link to='/#'>Home</Link>
-      <Link to='/new'>New</Link>
-
-    </div>
+    <styles.container>
+      <Link style={styles.link} to='/#'>Home</Link>
+      <Link style={styles.link} to='/new'>New</Link>
+    </styles.container>
   );
 }
 
